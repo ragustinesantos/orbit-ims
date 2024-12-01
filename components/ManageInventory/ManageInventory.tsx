acfcb3715404e7d0deb2d6a6294d3b385280ecb2
Raymond Santos <ragustinesantos@gmail.com>
2024-12-01 13:21:24 -0700

    12-01-2024 Update: - Implemented Search Functionality - Updated Number types in UpdateItem to String. Casting to Number/String happens before API call. Solves TextInput unable to clear '0'.

--- components/ManageInventory/ManageInventory.tsx
+++ components/ManageInventory/ManageInventory.tsx
@@ -1,26 +1,25 @@
 import { useEffect, useState } from 'react';
 import { IconChevronRight } from '@tabler/icons-react';
 import { Group, Tabs, Text } from '@mantine/core';
+import { defaultItem, defaultSupplier, Item, Supplier } from '@/app/_utils/schema';
+import { fetchCategories, fetchInventory, fetchSuppliers } from '@/app/_utils/utility';
 import AddItem from '../AddItem/AddItem';
 import DeleteItem from '../DeleteItem/DeleteItem';
 import SearchItem from '../SearchItem/SearchItem';
 import UpdateItem from '../UpdateItem/UpdateItem';
 import classes from './ManageInventory.module.css';
-import { fetchCategories, fetchInventory, fetchSuppliers } from '@/app/_utils/utility';
-import { defaultItem, defaultSupplier, Item, Supplier } from '@/app/_utils/schema';
 
 export default function ManageInventory() {
-
   const [inventory, setInventory] = useState<Item[]>([{ ...defaultItem }]);
   const [categoryList, setCategoryList] = useState<string[]>([]);
   const [supplierList, setSupplierList] = useState<Supplier[]>([{ ...defaultSupplier }]);
 
   // Retrieve database and enum information on page load
-    useEffect(() => {
-      fetchInventory(setInventory);
-      fetchSuppliers(setSupplierList);
-      fetchCategories(setCategoryList);
-    }, []);
+  useEffect(() => {
+    fetchInventory(setInventory);
+    fetchSuppliers(setSupplierList);
+    fetchCategories(setCategoryList);
+  }, []);
 
   return (
     <Tabs
@@ -35,33 +34,41 @@ export default function ManageInventory() {
     >
       <Tabs.List>
         <Tabs.Tab value="searchItem">
-          <Group classNames={{
-            root: classes.rootGroup
-          }}>
+          <Group
+            classNames={{
+              root: classes.rootGroup,
+            }}
+          >
             <Text>Search Item</Text>
             <IconChevronRight size={24} stroke={1.5} />
           </Group>
         </Tabs.Tab>
         <Tabs.Tab value="addItem">
-          <Group classNames={{
-            root: classes.rootGroup
-          }}>
+          <Group
+            classNames={{
+              root: classes.rootGroup,
+            }}
+          >
             <Text>Add Item</Text>
             <IconChevronRight size={24} stroke={1.5} />
           </Group>
         </Tabs.Tab>
         <Tabs.Tab value="updateItem">
-          <Group classNames={{
-            root: classes.rootGroup
-          }}>
+          <Group
+            classNames={{
+              root: classes.rootGroup,
+            }}
+          >
             <Text>Update Item</Text>
             <IconChevronRight size={24} stroke={1.5} />
           </Group>
         </Tabs.Tab>
         <Tabs.Tab value="deleteItem">
-          <Group classNames={{
-            root: classes.rootGroup
-          }}>
+          <Group
+            classNames={{
+              root: classes.rootGroup,
+            }}
+          >
             <Text>Delete Item</Text>
             <IconChevronRight size={24} stroke={1.5} />
           </Group>
@@ -69,7 +76,7 @@ export default function ManageInventory() {
       </Tabs.List>
 
       <Tabs.Panel value="searchItem">
-        <SearchItem />
+        <SearchItem inventory={inventory} />
       </Tabs.Panel>
       <Tabs.Panel value="addItem">
         <AddItem supplierList={supplierList} categoryList={categoryList} />
--- components/UpdateItem/UpdateItem.tsx
+++ components/UpdateItem/UpdateItem.tsx
@@ -35,9 +35,9 @@ export default function UpdateItem({
   const [packageUnit, setPackageUnit] = useState<string>('');
   const [supplyUnit, setSupplyUnit] = useState<string>('');
   const [supplierName, setSupplierName] = useState<string>('');
-  const [minPurchaseQty, setMinPurchaseQty] = useState<number>(0);
-  const [minStorageQty, setMinStorageQty] = useState<number>(0);
-  const [currentStockInStoreRoom, setCurrentStockInStoreRoom] = useState<number>(0);
+  const [minPurchaseQty, setMinPurchaseQty] = useState<string>('');
+  const [minStorageQty, setMinStorageQty] = useState<string>('');
+  const [currentStockInStoreRoom, setCurrentStockInStoreRoom] = useState<string>('');
 
   // Nullable states due to Select Mantine component handling
   const [category, setCategory] = useState<string | null>('');
@@ -47,10 +47,10 @@ export default function UpdateItem({
   const handleItemName = (newTxt: string) => setItemName(newTxt);
   const handlePackageUnit = (newTxt: string) => setPackageUnit(newTxt);
   const handleSupplyUnit = (newTxt: string) => setSupplyUnit(newTxt);
-  const handleMinPurchaseQty = (newTxt: string) => setMinPurchaseQty(Number(newTxt));
-  const handleMinStorageQty = (newTxt: string) => setMinStorageQty(Number(newTxt));
+  const handleMinPurchaseQty = (newTxt: string) => setMinPurchaseQty(newTxt);
+  const handleMinStorageQty = (newTxt: string) => setMinStorageQty(newTxt);
   const handleCurrentStockInStoreRoom = (newTxt: string) =>
-    setCurrentStockInStoreRoom(Number(newTxt));
+    setCurrentStockInStoreRoom(newTxt);
 
   // Handle update submit
   const handleSubmit = () => {
@@ -58,13 +58,13 @@ export default function UpdateItem({
       supplierId: supplierId || '',
       inventoryId: '',
       itemName,
-      currentStockInStoreRoom,
+      currentStockInStoreRoom: Number(currentStockInStoreRoom),
       packageUnit,
       supplyUnit,
       category: category || '',
-      isCritical: minStorageQty >= currentStockInStoreRoom,
-      isCriticalThreshold: minStorageQty,
-      minPurchaseQty,
+      isCritical: Number(minStorageQty) >= Number(currentStockInStoreRoom),
+      isCriticalThreshold: Number(minStorageQty),
+      minPurchaseQty: Number(minPurchaseQty),
     };
 
     // Send updated item for PUT
@@ -109,9 +109,9 @@ export default function UpdateItem({
       setPackageUnit(selectedItem.packageUnit || '');
       setSupplyUnit(selectedItem.supplyUnit || '');
       setCategory(selectedItem.category || '');
-      setMinPurchaseQty(selectedItem.minPurchaseQty || 0);
-      setMinStorageQty(selectedItem.isCriticalThreshold || 0);
-      setCurrentStockInStoreRoom(selectedItem?.currentStockInStoreRoom || 0);
+      setMinPurchaseQty(String(selectedItem.minPurchaseQty));
+      setMinStorageQty(String(selectedItem.isCriticalThreshold));
+      setCurrentStockInStoreRoom(String(selectedItem.currentStockInStoreRoom));
     };
 
     updateValues();
