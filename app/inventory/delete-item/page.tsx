6ce6498d1cf73745e4e995bf7e4f104e82bac65b
lunian16 <lijunxianjunction@gmail.com>
2025-01-10 14:59:20 -0700

    Update page.tsx

--- app/inventory/delete-item/page.tsx
+++ app/inventory/delete-item/page.tsx
@@ -2,21 +2,8 @@
 
 import { NavbarNested } from '@/components/NavbarNested/NavbarNested';
 import DeleteItem from '@/components/DeleteItem/DeleteItem';
-import { defaultItem, defaultSupplier, Item, Supplier } from '@/app/_utils/schema';
-import { fetchCategories, fetchInventory, fetchSuppliers } from '@/app/_utils/utility';
-import { useEffect, useState } from 'react';
 
 export default function DeleteItemPage() {
-    const [inventory, setInventory] = useState<Item[]>([{ ...defaultItem }]);
-    const [categoryList, setCategoryList] = useState<string[]>([]);
-    const [supplierList, setSupplierList] = useState<Supplier[]>([{ ...defaultSupplier }]);
-    const [refresh, setRefresh] = useState<number>(0);
-
-      useEffect(() => {
-        fetchInventory(setInventory);
-        fetchSuppliers(setSupplierList);
-        fetchCategories(setCategoryList);
-      }, [refresh]);
 
   return (
     <main style={{ display: 'flex', width: '100vw' }}>
