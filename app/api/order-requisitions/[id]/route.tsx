cbf302f884309ce67f12e65109d30a28f0a1b730
Raymond Santos <ragustinesantos@gmail.com>
2025-01-10 14:36:27 -0700

    01-10-2025 Update: - Created the schemas for order requisition, ror and ror template - Implemented the database services for order requisition, ror and ror template - Implemented the api routes for order requisition, ror and ror template - Minor update to AddItem: removed unnecessary <div> after <main>

---
+++ app/api/order-requisitions/[id]/route.tsx
@@ -0,0 +1,96 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { z } from 'zod';
+import {
+  dbDeleteEmployee,
+  dbGetEmployee,
+  dbUpdateEmployee,
+} from '@/app/_services/employees-service';
+import {
+  dbDeleteOrderRequisition,
+  dbGetOrderRequisition,
+  dbUpdateOrderRequisition,
+} from '@/app/_services/order-requisition-service';
+
+export async function GET(request: Request, { params }: { params: any }) {
+  try {
+    const { id } = await params;
+
+    const orderRequisition = await dbGetOrderRequisition(id);
+
+    return new Response(JSON.stringify(orderRequisition), { status: 200 });
+  } catch (error) {
+    return new Response(JSON.stringify({ error }), { status: 404 });
+  }
+}
+
+export async function PUT(request: Request, { params }: { params: any }) {
+  try {
+    const { id } = await params;
+    const updatedOrderRequisition = await request.json();
+
+    const orderRequisitionSchema = z.object({
+      requisitionId: z.string(),
+      requisitionType: z.string(),
+      requisitionDate: z.date(),
+      employeeId: z.string(),
+      approvalE2: z.string(),
+      approvalE3: z.string(),
+      approvalP1: z.string(),
+      isApprovedE2: z.boolean(),
+      isApprovedE3: z.boolean(),
+      isApprovedP1: z.boolean(),
+      recipientName: z.string(),
+      disposalPlan: z.string(),
+      remarks: z.string(),
+    });
+
+    const validatedOrderRequisition = orderRequisitionSchema.parse(updatedOrderRequisition);
+
+    await dbUpdateOrderRequisition(id, validatedOrderRequisition);
+    return new Response(null, { status: 204 });
+  } catch (error) {
+    return new Response(JSON.stringify({ error }), { status: 400 });
+  }
+}
+
+export async function PATCH(request: Request, { params }: { params: any }) {
+  try {
+    const { id } = await params;
+    const updatedOrderRequisition = await request.json();
+
+    const orderRequisitionSchema = z.object({
+      requisitionId: z.string().optional(),
+      requisitionType: z.string().optional(),
+      requisitionDate: z.date().optional(),
+      employeeId: z.string().optional(),
+      approvalE2: z.string().optional(),
+      approvalE3: z.string().optional(),
+      approvalP1: z.string().optional(),
+      isApprovedE2: z.boolean().optional(),
+      isApprovedE3: z.boolean().optional(),
+      isApprovedP1: z.boolean().optional(),
+      recipientName: z.string().optional(),
+      disposalPlan: z.string().optional(),
+      remarks: z.string().optional(),
+    });
+
+    const validatedOrderRequisition = orderRequisitionSchema.parse(updatedOrderRequisition);
+
+    await dbUpdateOrderRequisition(id, validatedOrderRequisition);
+    return new Response(null, { status: 204 });
+  } catch (error) {
+    return new Response(JSON.stringify({ error }), { status: 400 });
+  }
+}
+
+export async function DELETE(request: Request, { params }: { params: any }) {
+  try {
+    const { id } = await params;
+
+    await dbDeleteOrderRequisition(id);
+
+    return new Response(null, { status: 200 });
+  } catch (error) {
+    return new Response(JSON.stringify({ error }), { status: 400 });
+  }
+}
---
+++ app/api/rorTemplates/[id]/route.tsx
@@ -0,0 +1,80 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { z } from 'zod';
+import {
+  dbDeleteRecurringOrder,
+  dbGetRecurringOrder,
+  dbUpdateRecurringOrder,
+} from '@/app/_services/ror-service';
+import {
+  dbDeleteRorTemplate,
+  dbGetRorTemplate,
+  dbUpdateRorTemplate,
+} from '@/app/_services/ror-template-service';
+
+export async function GET(request: Request, { params }: { params: any }) {
+  try {
+    const { id } = await params;
+
+    const rorTemplate = await dbGetRorTemplate(id);
+
+    return new Response(JSON.stringify(rorTemplate), { status: 200 });
+  } catch (error) {
+    return new Response(JSON.stringify({ error }), { status: 404 });
+  }
+}
+
+export async function PUT(request: Request, { params }: { params: any }) {
+  try {
+    const { id } = await params;
+    const updatedRorTemplate = await request.json();
+
+    const rorTemplateSchema = z.object({
+      rorTemplateId: z.string(),
+      templateName: z.string(),
+      itemList: z.array(z.string()),
+      isTemplateApprovedE2: z.boolean(),
+      isTemplateApprovedE3: z.boolean(),
+    });
+
+    const validatedRorTemplate = rorTemplateSchema.parse(updatedRorTemplate);
+
+    await dbUpdateRorTemplate(id, validatedRorTemplate);
+    return new Response(null, { status: 204 });
+  } catch (error) {
+    return new Response(JSON.stringify({ error }), { status: 400 });
+  }
+}
+
+export async function PATCH(request: Request, { params }: { params: any }) {
+  try {
+    const { id } = await params;
+    const updatedRorTemplate = await request.json();
+
+    const rorTemplateSchema = z.object({
+      rorTemplateId: z.string().optional(),
+      templateName: z.string().optional(),
+      itemList: z.array(z.string()).optional(),
+      isTemplateApprovedE2: z.boolean().optional(),
+      isTemplateApprovedE3: z.boolean().optional(),
+    });
+
+    const validatedRorTemplate = rorTemplateSchema.parse(updatedRorTemplate);
+
+    await dbUpdateRorTemplate(id, validatedRorTemplate);
+    return new Response(null, { status: 204 });
+  } catch (error) {
+    return new Response(JSON.stringify({ error }), { status: 400 });
+  }
+}
+
+export async function DELETE(request: Request, { params }: { params: any }) {
+  try {
+    const { id } = await params;
+
+    await dbDeleteRorTemplate(id);
+
+    return new Response(null, { status: 200 });
+  } catch (error) {
+    return new Response(JSON.stringify({ error }), { status: 400 });
+  }
+}
