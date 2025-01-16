fadfe286e1d4545c13557a48174a48e1ec5e3112
kyledyck@outlook.com <144828203+KyleJamesD@users.noreply.github.com>
2025-01-16 15:46:31 -0700

    Added more columns to Odor Table and added ODOR Title +  CSS styling

--- app/odor/page.tsx
+++ app/odor/page.tsx
@@ -2,23 +2,14 @@
 
 import { NavbarNested } from '@/components/NavbarNested/NavbarNested';
 import OdorComponent from '@/components/Odor/Odor';
-
+import classnames from './odorpage.module.css';
+import { Text } from '@mantine/core';
 
 export default function OdorPage() {
   return (
     <main style={{ display: 'flex', width: '100vw' }}>
       <NavbarNested />
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          width: '100%',
-          minWidth: '50vw',
-          //height: '100vh',
-          border: '1px solid red',
-        }}
-      >
+      <div>
         <OdorComponent/>
       </div>
     </main>
