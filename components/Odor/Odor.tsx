780e1be810b17b49f3af7c634a2058e2ce4c8d2a
kyledyck@outlook.com <144828203+KyleJamesD@users.noreply.github.com>
2025-01-18 13:38:04 -0700

    Added Decrement function and remove button/delete

--- components/Odor/Odor.tsx
+++ components/Odor/Odor.tsx
@@ -18,67 +18,70 @@ export default function OdorComponent() {
     const [searchValue, setSearchValue] = useState<string | null>('');
     const { inventory, supplierList, setCurrentPage, setCurrentSection } = useInventory();
 
-
-      //Get item object from inventory and assign it to itemOrders Array
-      function handleAddItem () {
-        console.log('handle add item pressed')
-        console.log(searchValue)
-        const matchedItem = inventory?.find((item) => item.itemId === searchValue);
-        console.log(matchedItem)
-        if (itemOrders.find((item)=>item.itemId === matchedItem?.itemId)) {
-                console.log('Item is already in array!')
-              }
-        else {
-            const newItem: ItemOrder = {
-            itemId: (matchedItem) ? matchedItem.itemId : "",
-            orderQty: 1,
-            pendingQty: 1,
-            servedQty: 0,
-            };
-            setitemOrders(prevOrders => [...prevOrders, newItem]);
+    //Get item object from inventory and assign it to itemOrders Array
+    function handleAddItem () {
+      console.log('handle add item pressed')
+      console.log(searchValue)
+      const matchedItem = inventory?.find((item) => item.itemId === searchValue);
+      console.log(matchedItem)
+      if (itemOrders.find((item)=>item.itemId === matchedItem?.itemId)) {
+              console.log('Item is already in array!')
             }
-
-      }
-        
-    
-
+      else {
+          const newItem: ItemOrder = {
+          itemId: (matchedItem) ? matchedItem.itemId : "",
+          orderQty: 1,
+          pendingQty: 1,
+          servedQty: 0,
+          };
+          setitemOrders(prevOrders => [...prevOrders, newItem]);
+          }
+    }
+  
     // To log the item orders array to the console to see the updated state
     useEffect(() => {
-        console.log('Updated itemOrders:', itemOrders);
-      }, [itemOrders]);
+      console.log('Updated itemOrders:', itemOrders);
+    }, [itemOrders]);
 
 
-      function increment (id : string) {
-        setitemOrders((prevItems) =>
-          prevItems.map((item) =>
-              item.itemId === id ? { ...item, orderQty: item.orderQty + 1 } : item
-          )
-      );
-        
-        
+    function increment (id : string) {
+      setitemOrders((prevItems) =>
+        prevItems.map((item) => item.itemId === id ? 
+        { ...item, orderQty: item.orderQty + 1 } : item));
+    }
 
+    function decrement (id : string) {
+      setitemOrders((prevItems) =>
+        prevItems.map((item) => item.itemId === id && item.orderQty > 1 ? 
+        { ...item, orderQty: item.orderQty - 1 } : item));
+    }
 
-      }
+    function handleRemoveItem (item : ItemOrder) {
+      let position = itemOrders.indexOf(item);
+      itemOrders.splice(position,1);
+      setitemOrders([...itemOrders]);
 
-
-
+    }
 
         const rows = itemOrders.map((item) => {
         const odorItem = inventory?.find((inv)=> inv.itemId === item.itemId)
         const supplier = supplierList?.find((inv)=> inv.supplierId === odorItem?.supplierId)
         return (
           <Table.Tr key={odorItem?.itemId}>
-            <Table.Td style={{ maxWidth: '30px', overflowX: 'scroll', scrollbarWidth: 'none' }}>{odorItem?.itemId}</Table.Td>
+            <Table.Td style={{maxWidth: '100px', overflowX: 'scroll', scrollbarWidth: 'none' }}>{odorItem?.itemId}</Table.Td>
             <Table.Td>{odorItem?.itemName}</Table.Td>
             <Table.Td>{odorItem?.category}</Table.Td>
             <Table.Td>{odorItem?.supplyUnit}</Table.Td>
             <Table.Td>{odorItem?.packageUnit}</Table.Td>
             <Table.Td>{supplier?.supplierName} </Table.Td>
             <Table.Td>
-              <Button variant="filled" size="xs" radius="md" classNames={{root:classnames.buttonSub}}>-</Button>
+              <Button classNames={{root:`${classnames.buttonDecrement} ${classnames.button}`}} onClick={()=>(decrement(item.itemId))} variant="filled" size="xs" radius="md" >-</Button>
               {item.orderQty}
-              <Button onClick={()=>(increment(item.itemId))} variant="filled" size="xs" radius="md" classNames={{root:classnames.buttonAdd}}>+</Button>
-              </Table.Td>
+              <Button classNames={{root:`${classnames.buttonIncrement} ${classnames.button}`}} onClick={()=>(increment(item.itemId))} variant="filled" size="xs" radius="md" >+</Button>
+            </Table.Td>
+            <Table.Td>
+              <Button classNames={{root:`${classnames.buttonRemove}`}} onClick={()=>(handleRemoveItem(item))} variant="filled" size="xs" radius="md">Remove</Button>
+            </Table.Td>
           </Table.Tr>
         ) 
       });
@@ -86,25 +89,11 @@ export default function OdorComponent() {
     return (
     <div  style={{ border: '1px solid red'}}>
       <Text classNames={{root: classnames.odorText,}}>On Demand Order Requisition</Text>
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          width: '83vw',
-          height:'85vh',
-          minWidth: '50vw',
-          //height: '100vh',
-          border: '1px solid blue',
-        }}>
-          <div style={{
-          alignItems: 'center',
-          justifyContent: 'center',
-          border: '1px solid blue',
-        }}>
+      <div className={classnames.exteriorDiv}>
+          <div className={classnames.interiorDiv}>
               <Text classNames={{root: classnames.rootText,}}>Add Items</Text>
 
-              <div  style={{ display:'flex', border: '1px solid red', width: '50vw', justifyContent: 'space-evenly'}}>
+              <div  className={classnames.searchItemDiv}>
                     <Select
                     label="Search Item"
                     placeholder="Select an item from the list..."
@@ -150,6 +139,7 @@ export default function OdorComponent() {
                       <Table.Th>Package Unit</Table.Th>
                       <Table.Th>Supplier</Table.Th>
                       <Table.Th>QTY</Table.Th>
+                      <Table.Th></Table.Th>
                     </Table.Tr>
                   </Table.Thead>
                   <Table.Tbody>{rows}</Table.Tbody>
