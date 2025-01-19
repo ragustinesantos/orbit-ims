688e8708666d3929c631d2b348739c620dd1b2c6
kyledyck@outlook.com <144828203+KyleJamesD@users.noreply.github.com>
2025-01-18 19:03:46 -0700

    daily commit

--- components/Odor2/Odor2.tsx
+++ components/Odor2/Odor2.tsx
@@ -8,10 +8,35 @@ import classnames from './odor2.module.css';
 import { useInventory } from '@/app/_utils/inventory-context';
 import { number } from 'zod';
 import { ItemOrder } from '@/app/_utils/schema';
+import { NewItemOrder } from '@/app/_utils/schema';
+
+interface setpropstype  {
+  newItemOrders: NewItemOrder[];
+  setNewItemOrders: React.Dispatch<React.SetStateAction<NewItemOrder[]>>;
+}
+
+export default function OdorComponent2({newItemOrders,setNewItemOrders}: setpropstype) {
+
+  const [showTemplate, setshowTemplate] = useState<boolean>(false)
+
+
+  function handleShowTemplate () {
+    setshowTemplate(true);
+  }
+
+
+  const template = (
+
+          <div>well hello there!</div>
 
 
 
-export default function OdorComponent2() {
+    );
+
+
+
+
+
 
 
     return (
@@ -20,9 +45,11 @@ export default function OdorComponent2() {
       <div className={classnames.exteriorDiv}>
           <div className={classnames.interiorDiv}>
               <Text classNames={{root: classnames.rootText,}}>Order Non Inventory Item</Text>
-          </div> 
-          </div>   
-        </div>
+              <Text>Would you like to order a item that is not in the Inventory?</Text>
+          </div>
+          {showTemplate ? template : <Button onClick={handleShowTemplate} >Yes</Button>} 
+      </div>   
+    </div>
 
     );
   }
