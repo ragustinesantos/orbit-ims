6f1aa205aca5a9a03a3e5bedcf7fde62eea99844
Markus Luis Flores <markuslsflores@gmail.com>
2025-01-25 00:37:55 -0700

    Make progress bar dynamic

--- components/WizardProgress/WizardProgress.tsx
+++ components/WizardProgress/WizardProgress.tsx
@@ -1,57 +1,80 @@
+import { useEffect, useState } from 'react';
 import classes from './WizardProgress.module.css';
 
 
 export default function WizardProgress() {
 
+    const [progressBar, setProgressBar] = useState(<div />);
+    const steps: String[] = ['Template', 'Order', 'Confirmation', 'Summary'];
+    const currentStep = 2;
+
+    const progressBarType = (rendered: boolean) => {
+        if (rendered) {
+            return (
+                <div className={classes.barContainer}>
+                    <div className={classes.progressBar}></div>
+                </div>
+            );
+        }
+        else {
+            return (
+                <div className={classes.barContainer}>
+                    <div className={classes.awaitingBar}>
+                        ||||||||||||||||||||
+                    </div>
+                </div>
+            )
+        }
+    }
+
+    const stepType = (rendered: boolean, stepName: String, index: number) => {
+        if (rendered) {
+            return (
+                <div className={classes.stepContainer}>
+                    <div className={classes.progressCircle}>
+                        {index + 1}
+                    </div>
+                    <div className={classes.stepTitle}>
+                        {stepName}
+                    </div>
+                </div>
+            );
+        }
+        else {
+            return (
+                <div className={classes.stepContainer}>
+                    <div className={classes.awaitingCircle}>
+                        {index + 1}
+                    </div>
+                    <div className={classes.stepTitle}>
+                        {stepName}
+                    </div>
+                </div>
+            );
+        }
+    }
+
+    useEffect(() => {
+        setProgressBar(
+            <>
+                {
+                    steps.map((step, index) => {
+                        let currentStepDisplay = [];
+                        if (index > 0) {
+                            currentStepDisplay.push(progressBarType(index < currentStep));
+                        }
+                        currentStepDisplay.push(stepType(index < currentStep, step, index));
+
+                        return currentStepDisplay;
+                    })
+                }
+            </>
+        )
+    }, []);
+
     return (
         <div className={classes.header}>
-            <div className={classes.stepContainer}>
-                <div className={classes.progressCircle}>
-                    1
-                </div>
-                <div className={classes.stepTitle}>
-                    Template
-                </div>
-            </div>
-            <div className={classes.barContainer}>
-                <div className={classes.progressBar}>
-
-                </div>
-            </div>
-            <div className={classes.stepContainer}>
-                <div className={classes.progressCircle}>
-                    2
-                </div>
-                <div className={classes.stepTitle}>
-                    Order
-                </div>
-            </div>
-            <div className={classes.barContainer}>
-                <div className={classes.awaitingBar}>
-                    ||||||||||||||||||||
-                </div>
-            </div>
-            <div className={classes.stepContainer}>
-                <div className={classes.awaitingCircle}>
-                    3
-                </div>
-                <div className={classes.stepTitle}>
-                    Confirmation
-                </div>
-            </div>
-            <div className={classes.barContainer}>
-                <div className={classes.awaitingBar}>
-                    ||||||||||||||||||||
-                </div>
-            </div>
-            <div className={classes.stepContainer}>
-                <div className={classes.awaitingCircle}>
-                    4
-                </div>
-                <div className={classes.stepTitle}>
-                    Summary
-                </div>
-            </div>
+            {progressBar}
         </div>
     );
 }
