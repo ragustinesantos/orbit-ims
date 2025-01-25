1eaa502835f5090cf5883911d7c988c2f75c7882
Markus Luis Flores <markuslsflores@gmail.com>
2025-01-24 23:20:35 -0700

    Progress bar title

--- components/WizardProgress/WizardProgress.tsx
+++ components/WizardProgress/WizardProgress.tsx
@@ -5,32 +5,52 @@ export default function WizardProgress() {
 
     return (
         <div className={classes.header}>
-            <div className={classes.progressCircle}>
-                1
+            <div className={classes.stepContainer}>
+                <div className={classes.progressCircle}>
+                    1
+                </div>
+                <div className={classes.stepTitle}>
+                    Template
+                </div>
             </div>
             <div className={classes.barContainer}>
                 <div className={classes.progressBar}>
 
                 </div>
             </div>
-            <div className={classes.progressCircle}>
-                2
+            <div className={classes.stepContainer}>
+                <div className={classes.progressCircle}>
+                    2
+                </div>
+                <div className={classes.stepTitle}>
+                    Order
+                </div>
             </div>
             <div className={classes.barContainer}>
                 <div className={classes.awaitingBar}>
                     ||||||||||||||||||||
                 </div>
             </div>
-            <div className={classes.awaitingCircle}>
-                3
+            <div className={classes.stepContainer}>
+                <div className={classes.awaitingCircle}>
+                    3
+                </div>
+                <div className={classes.stepTitle}>
+                    Confirmation
+                </div>
             </div>
             <div className={classes.barContainer}>
                 <div className={classes.awaitingBar}>
                     ||||||||||||||||||||
                 </div>
             </div>
-            <div className={classes.awaitingCircle}>
-                4
+            <div className={classes.stepContainer}>
+                <div className={classes.awaitingCircle}>
+                    4
+                </div>
+                <div className={classes.stepTitle}>
+                    Summary
+                </div>
             </div>
         </div>
     );
