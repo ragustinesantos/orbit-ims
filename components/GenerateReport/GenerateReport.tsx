15552aa470009cf39eec6b5154ad09a82b212117
lunian16 <lijunxianjunction@gmail.com>
2025-03-14 02:55:11 -0600

    Update GenerateReport.tsx

--- components/GenerateReport/GenerateReport.tsx
+++ components/GenerateReport/GenerateReport.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import {Container,Text, Button, Select, Table, TextInput, Paper, Divider, Notification, Flex} from "@mantine/core";
 import {
   fetchStockInOrders,
