a07df7e0de6d83bd2e64394d6ed6dd1968a577fe
kyledyck@outlook.com <144828203+KyleJamesD@users.noreply.github.com>
2025-03-15 15:34:16 -0600

    This branch now has the basic functionality for the email notificaitons, must move out of profile page and into Create new ror and E3 access

--- components/ProfilePage/ProfilePage.tsx
+++ components/ProfilePage/ProfilePage.tsx
@@ -3,10 +3,15 @@ import { Button, Group, Select, SimpleGrid, Text, TextInput, Avatar, Flex } from
 import  classnames  from './ProfilePage.module.css';
 import { useInventory } from '@/app/_utils/inventory-context';
 import { useUserAuth } from '@/app/_utils/auth-context';
+import FormData from 'form-data';
+import Mailgun from 'mailgun.js';
 
 
 
 export default function ProfilePage () {
+
+    const reqid = "439n&*hbdw78923*^&^%98dH&*A!WHBdvg9"
+
 
 
  
@@ -21,6 +26,28 @@ const { currentEmployee } = useInventory();
         }
         window.location.replace("/")
     };
+
+    async function sendNewPOMsg(reqid : string) {
+        const mailgun = new Mailgun(FormData);
+        const mg = mailgun.client({
+          username: "api",
+          key: process.env.NEXT_PUBLIC_MAILGUN_API_KEY || "API_KEY",
+          // When you have an EU-domain, you must specify the endpoint:
+          // url: "https://api.eu.mailgun.net/v3"
+        });
+        try {
+          const data = await mg.messages.create("sandbox890f9fe65f974e4ca66405364dc99b84.mailgun.org", {
+            from: "OrbitIMS@sandbox890f9fe65f974e4ca66405364dc99b84.mailgun.org",
+            to: ["orbit.imsystem@gmail.com","kyle_1990@outlook.com"],
+            subject: "New Requisition Approval",
+            text: `Requisisitons ID# "${reqid}" has recieved E3 Approval and is ready for PO creation! \n\n Thank you.`,
+          });
+      
+          console.log(data); // logs response data
+        } catch (error) {
+          console.log(error); //logs any error
+        }
+      }
 
     return (
         
@@ -45,12 +72,18 @@ const { currentEmployee } = useInventory();
                             <div className={classnames.title}>Department:</div>
                             <div>{currentEmployee?.department}</div>
                             <div className={classnames.title}>Access Level:</div>
-                            <div>{currentEmployee?.employeeLevel}</div>
+                            <div>{currentEmployee?.employeeLevel[0] ? currentEmployee?.employeeLevel[0] + ", " : ""} 
+                                 {currentEmployee?.employeeLevel[1] ? currentEmployee?.employeeLevel[1] + ", " : ""}  
+                                 {currentEmployee?.employeeLevel[2] ? currentEmployee?.employeeLevel[2] + ", " : ""} 
+                                 {currentEmployee?.employeeLevel[3] ? currentEmployee?.employeeLevel[3] + ", " : ""} 
+                                 {currentEmployee?.employeeLevel[4] ? currentEmployee?.employeeLevel[4] + ", " : ""} 
+                                 {currentEmployee?.employeeLevel[5] ? currentEmployee?.employeeLevel[5] : ""}</div>
                             
                         </SimpleGrid>
                         <div>
                         <Button variant="filled" classNames={{root:classnames.button}}>Change Password</Button>
                         <Button variant="filled" onClick={handleLogout} classNames={{root:classnames.button}}>Logout</Button>
+                        <Button variant="filled" onClick={() =>sendNewPOMsg(reqid)}  classNames={{root:classnames.button}}>send email</Button>
                         </div>  
                     </div>
             </div>
