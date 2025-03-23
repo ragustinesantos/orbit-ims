e7e16c6d1fa7a52ba7549c8820a555141b327a6a
Markus Luis Flores <markuslsflores@gmail.com>
2025-03-22 22:16:22 -0600

    Remove playwright from jest

--- jest.config.cjs
+++ jest.config.cjs
@@ -11,6 +11,7 @@ const customJestConfig = {
     '^@/pages/(.*)$': '<rootDir>/pages/$1',
   },
   testEnvironment: 'jest-environment-jsdom',
+  testPathIgnorePatterns: ['<rootDir>/playwright-tests/'],
 };
 
 module.exports = createJestConfig(customJestConfig);
