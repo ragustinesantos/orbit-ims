a4ed6f71c97e465efa27c04978abdb7e728a3799
kyledyck@outlook.com <144828203+KyleJamesD@users.noreply.github.com>
2025-03-24 11:12:14 -0600

    Fix Search value, change from itemname to ItemID

--- components/SearchItem/SearchItem.tsx
+++ components/SearchItem/SearchItem.tsx
@@ -15,9 +15,10 @@ export default function SearchItem() {
   const toggleImgModalState = (itemId: string) => {
     setModalStateTracker((prev) => ({ ...prev, [itemId]: !prev[itemId] }));
   };
+  console.log(searchValue)
 
   const rows = inventory?.map((item) => {
-    return item.itemName.includes(searchValue || '') ? (
+    return item.itemId.includes(searchValue || '') ? (
       <Table.Tr key={item.itemId}>
         <ImgModal
           item={item}
@@ -72,7 +73,7 @@ export default function SearchItem() {
               label="Search Item"
               placeholder="Select an item from the list..."
               data={inventory?.map((item) => ({
-                value: item.itemName,
+                value: item.itemId,
                 label: item.itemName,
               }))}
               allowDeselect
